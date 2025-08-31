11479e3c08a2d096b1f4ab8dc0e9e45484540bc1
Sammie1227 <rosalsamuel1227@gmail.com>
2025-08-31 11:40:41 +0800

    rtnadom

--- game/7days/game/lights_control.gd
+++ game/7days/game/lights_control.gd
@@ -17,7 +17,7 @@ extends Node3D
 }
 
 func _ready() -> void:
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(3.0).timeout
 	_light_room("living_room","_all_",false)
 	await get_tree().create_timer(1.0).timeout
 	_light_room("living_room","_all_",true)
