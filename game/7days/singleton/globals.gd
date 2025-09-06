17c3d72da708cfab5ae49163f263750d3a61b6f0
Sammie1227 <rosalsamuel1227@gmail.com>
2025-09-06 23:04:18 +0800

    new : game

--- game/7days/singleton/globals.gd
+++ game/7days/singleton/globals.gd
@@ -1,1 +1,14 @@
 extends Node
+
+var current_day : int = 1
+
+var days : Dictionary = {
+	1:{"action": "intro"},
+	2:{"action": "random_anomaly"},
+	3:{"action": "random_anomaly"},
+	4:{"action": "random_anomaly"},
+	5:{"action": "random_anomaly"},
+	6:{"action": "random_anomaly"},
+	7:{"action": "ending"},
+	
+}
